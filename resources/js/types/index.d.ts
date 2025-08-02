ecb28ae50bc64d9162070c86c1a236b823bf3861
HimeshDua <himeshdua22@gmail.com>
2025-08-02 16:51:51 +0500

    updates type and theme

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -64,11 +64,9 @@ export interface VendorService {
     vendorLogo: string;
     location: string;
     connectionType: 'fiber' | 'dsl' | 'wireless';
-    serviceType: 'internet' | 'vpn' | 'dedicated-line' | 'hosting';
     price: string;
     postedDate: string;
     description: string;
-    featured: boolean;
     highlight: 'new' | 'trending' | 'reliable' | 'popular' | undefined;
     features: string[];
 }
